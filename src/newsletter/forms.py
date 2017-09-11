d5be40ccd733df9ab3461a481839571ced5064b0
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-09-11 20:42:17 +0600

    added form validation

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -6,3 +6,15 @@ class SignUpForm(forms.ModelForm):
         model = SignUp
         fields = ['full_name','email']
         # exclude =['full_name']
+    def clean_email(self):
+        email= self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain,extension = provider.split(".")
+        if not domain=='USC':
+            raise forms.ValidationError("Please make sure u use ur USC email")
+        if not extension=='edu':
+            raise forms.ValidationError("Please use a valid edu email address")
+        return email
+
+    def clean_full_name(self):
+        full
