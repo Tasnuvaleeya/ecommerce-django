865250733a8be18bc95432a409af91b54235c9cc
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-09-12 20:12:51 +0600

    Custom form added

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import SignUp
 
+class ContactForm(forms.Form):
+    full_name = forms.CharField(required=False)
+    email = forms.EmailField()
+    message = forms.CharField()
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['full_name','email']
         # exclude =['full_name']
+
     def clean_email(self):
         email= self.cleaned_data.get('email')
         email_base, provider = email.split("@")
@@ -16,3 +22,6 @@ class SignUpForm(forms.ModelForm):
             raise forms.ValidationError("Please use a valid edu email address")
         return email
 
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get("full_name")
+        return full_name
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SignUpForm
+from .forms import SignUpForm, ContactForm
 
 
 def home(request):
@@ -9,10 +9,7 @@ def home(request):
         "title": title,
         "form": form
     }
-    # if request.method == "POST":
-    #     print request.POST
-    # if request.user.is_authenticated():
-    #     title = "My Title %s" %(request.user)
+
     if form.is_valid():
         # form.save()
         instance = form.save(commit=False)
@@ -22,10 +19,26 @@ def home(request):
         instance.full_name = full_name
         #
         instance.save()
-        # print instance
-        # print instance.timestamp
+
         context ={
             "title": "Thank You"
         }
 
     return render(request, 'home.html', context)
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        for key,value in form.cleaned_data.iteritems():
+            print key,value
+            # print form.cleaned_data.get(key)
+        # email = form.cleaned_data.get("email")
+        # full_name = form.cleaned_data.get("full_name")
+        # message = form.cleaned_data.get("message")
+        # print email,message,full_name
+
+    context ={
+        "form" :form,
+    }
+    return render(request,"forms.html",context)
