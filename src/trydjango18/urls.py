c2ca3503ec183377875e6e7fccf092b7ee078140
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-09-13 18:30:18 +0600

    URL name used as link

--- src/trydjango18/urls.py
+++ src/trydjango18/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
+    url(r'^about/$', 'trydjango18.views.about', name='about'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
 if settings.DEBUG:
-
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
